f6b4127567cbddd7f07ad75512be8c8b6d7ee78c
Vantria <s1spam118@gmail.com>
2023-04-19 21:54:03 +0100

    add basic header

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import HistoryPage from "./pages/HistoryPage/HistoryPage";
 
+// Components
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,15 @@
 import "./Header.scss";
+import { Link } from "react-router-dom";
+import holoBall from "../../assets/illustrations/holoball.png";
 
 const Header = () => {
-  return <></>;
+  return (
+    <header className="header">
+      <Link to="/" className="header__link">
+        InterAI
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
