2662280f23dd9b4f389f9787d6e60d38a5886b1a
Vantria <s1spam118@gmail.com>
2023-04-19 20:34:02 +0100

    add base files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import HistoryPage from "./pages/HistoryPage/HistoryPage";
 
 function App() {
-  return <h1>HELLO WORLD</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/HistoryPage/HistoryPage.js
@@ -0,0 +1,7 @@
+import "./HistoryPage.scss";
+
+const HistoryPage = () => {
+  return <></>;
+};
+
+export default HistoryPage;
