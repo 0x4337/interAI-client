26b9f22eb76d2dfcf227cef653c8a063a3ba7108
Vantria <s1spam118@gmail.com>
2023-04-22 15:35:40 +0100

    add working whisper route

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -6,7 +6,7 @@ import glassPanels from "../../assets/objects/glassPanels.mp4";
 
 const HomePage = () => {
   return (
-    <hero className="home">
+    <section className="home">
       {/* <img
         className="home__graphic"
         src={graphic1}
@@ -15,21 +15,21 @@ const HomePage = () => {
       <video className="home__video" autoPlay loop muted>
         <source src={glassPanels} type="video/mp4" />
       </video>
-      <section className="brief">
+      <div className="brief">
         <h1 className="brief__title">InterAI</h1>
 
         <p className="brief__description">
           InterAI is a platform that allows you to practice your interview
           skills with dynamic, AI powered questions and responses.
         </p>
-      </section>
+      </div>
 
-      <section className="main">
+      <div className="main">
         <Link to="/interview" className="main__start">
           Start Interview
         </Link>
-      </section>
-    </hero>
+      </div>
+    </section>
   );
 };
 
--- src/pages/InterviewPage/InterviewPage.js
+++ src/pages/InterviewPage/InterviewPage.js
@@ -1,10 +1,15 @@
 import "./InterviewPage.scss";
 import { useState } from "react";
+import RecordRTC, { invokeSaveAsDialog } from "recordrtc";
+
+import axios from "axios";
 
 import morph from "../../assets/objects/morph.mp4";
 
 const InterviewPage = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [recorder, setRecorder] = useState(null);
+  const [recording, setRecording] = useState(false);
 
   const handleCategorySelect = (category) => {
     setSelectedCategory(category);
@@ -16,6 +21,55 @@ const InterviewPage = () => {
       (selectedCategory === category ? " info__category--selected" : "")
     );
   };
+
+  const startRecording = () => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const audioRecorder = RecordRTC(stream, {
+          type: "audio",
+          mimeType: "audio/webm",
+        });
+        audioRecorder.startRecording();
+        setRecorder(audioRecorder);
+        setRecording(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const stopRecording = async () => {
+    recorder.stopRecording(async () => {
+      //   invokeSaveAsDialog(recorder.getBlob(), "audio.webm");
+      setRecording(false);
+
+      // Sending recorded audio to the server
+      const formData = new FormData();
+      formData.append("audio", recorder.getBlob(), "audio.webm");
+
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/api/openai/generate`,
+          formData
+        );
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
+  // TODO:
+  // 1. When a user clicks start, check what category they selected.
+  // 2. Depending on their selected category, prompt GPT-4 to start an interview based around that category.
+  // 3. Feed the GPT-4 output to Eleven Labs API to generate speech.
+  // 4. Play the speech to the user.
+  // 5. Add a record button to record the user's response.
+  // 6. Take the recorded response and transform it into a .mp3 file.
+  // 7. Feed the .mp3 file to OpenAI's Whisper api to generate a transcript.
+  // 8. Feed the transcript to GPT-4 to generate a response.
+  // 9. Repeat steps 3-8 until the user is done with the interview.
 
   return (
     <section className="interview">
@@ -60,8 +114,9 @@ const InterviewPage = () => {
           className={
             "start__button" + (selectedCategory ? " start__button--active" : "")
           }
+          onClick={recording ? stopRecording : startRecording}
         >
-          Start
+          {recording ? "Stop Recording" : "Start Recording"}
         </button>
       </div>
     </section>
